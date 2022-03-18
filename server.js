f432a2865906374768f3c352cbc766ac3aba4820
MuhammadYudhistira <myudhistira07@gmail.com>
2022-03-18 14:16:19 +0700

    update server

--- server.js
+++ server.js
@@ -1,26 +1,19 @@
 const express = require('express');
 const app = express()
 
-app.set("view engine", "ejs")
-
 app.get('/',(req,res) =>{
     res.send("HomePage")
     res.send("Test")
 });
 
-<<<<<<< HEAD
 app.get('/mencetak',(req,res) =>{
     res.send("laporan")
 });
 
 app.get('/menambahkan',(req,res) =>{
     res.send("CPMK")
-=======
-app.get('/mengubah',(req,res) =>{
-    res.send("CPMK mata kuliah")
->>>>>>> 194fddf308425899ea711d5eda9610b29eed6972
 });
-=========
+
  app.get('/mengeksport',(req,res) =>{
      res.send("RPS jadi PDF")
  });
