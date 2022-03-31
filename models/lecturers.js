26c0b6af2c7c9bb3ff895ca64bf078ec6c313c83
gitaptr <2011523011_gita@student.unand.ac.id>
2022-03-31 20:08:00 +0700

    Database Lectures

---
+++ models/lecturers.js
@@ -0,0 +1,33 @@
+const { Sequelize, DataTypes, err } = require('sequelize');
+const sequelize = new Sequelize("mysql://root@localhost/loki")
+
+sequelize.define('Lectures', {
+    id : {
+        type : DataTypes.BIGINT,
+        autoIncrement : true,
+        primaryKey : true
+    },
+    name : {
+        type : DataTypes.STRING,
+        allowNull : false
+    },
+    reg_id: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    phone : {
+        type : DataTypes.STRING,
+        allowNull: false
+    },
+    status :  {
+        type : DataTypes.INTEGER,
+        allowNull: false
+    },
+    created_at : {
+        type : DataTypes.DATE
+    
+    },
+    update_at : {
+        type : DataTypes.DATE
+    
+    });
