dc27dc5223af8edae33323f90dc44e929fb46141
ayunathan <2011523009_nathaniarahayu@student.unand.ac.id>
2022-03-31 20:36:19 +0700

    database curricula

---
+++ models/curricula.js
@@ -0,0 +1,37 @@
+const { Sequelize, DataTypes, err } = require('sequelize');
+const sequelize = new Sequelize("mysql://root@localhost/loki")
+
+sequelize.define('curricula', {
+    id : {
+        type : DataTypes.BIGINT,
+        allowNull : false,
+        primaryKey : true
+    },
+    name : {
+        type : DataTypes.STRING,
+        allowNull : false,
+    },
+    active : {
+        type : DataTypes.STRING,
+        allowNull : false,
+    },
+    year_start : {
+        type : DataTypes.INTEGER,
+        allowNull : false,
+    },
+    year_end : {
+        type : DataTypes.INTEGER,
+        allowNull : false,
+    },
+    description : {
+        type : DataTypes.STRING,
+        allowNull : true,
+    },
+    created_at : {
+        type : DataTypes.DATE     
+    },
+    updated_at : {
+        type : DataTypes.DATE
+    }    
+
+});
