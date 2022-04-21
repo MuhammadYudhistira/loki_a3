f8cf4a87964c6f13e264f955c864bc21fb01794b
Muhammad Yudhistira <myudhistira07@gmail.com>
2022-04-21 21:29:56 +0700

    JWT

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,6 @@ require('dotenv').config()
 const express = require('express');
 const router = express.Router()
 const jwt = ('jsonwebtoken')
-const auth = require('../middleware/authenticationToken')
 
 router.get('/', (req,res) => {
     res.send("admin")
